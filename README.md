211e0767ceaba151496f619e86e97adccd68b5f8
Maria Snegireva <mariyasn88@gmail.com>
2024-05-27 18:23:55 +0300

    add game

--- README.md
+++ README.md
@@ -1,4 +1,72 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+# 2048 Game Implementation
+
+### Overview
+
+This project is an implementation of the popular 2048 game. The game logic is developed using JavaScript, and the user interface is built with HTML and CSS. Players can interact with the game using keyboard arrow keys to combine tiles and reach the 2048 tile.
+
+[DEMO LINK](https://mariasnegireva.github.io/2048_game/)
+
+### Project Structure
+
+The project consists of the following key components:
+
+- HTML and CSS: Pre-written templates for the game interface.
+- JavaScript Game Logic: Implemented in the src/modules/Game.class.js file.
+- Main Script: Integrates the game logic with the UI, found in the main.js script.
+
+### Game Features
+
+1. Game Board: A 4x4 grid where each cell can contain numbers like 2, 4, 8, ... 2^n.
+2. Tile Movement: Players can use arrow keys to move tiles in four directions.
+3. Tile Merging: When two tiles with the same number collide, they merge into a tile with the doubled number.
+4. Random Tile Generation: After each move, a new tile (2 or 4) appears in a random empty cell.
+5. Win Condition: The game displays a win message when a tile with 2048 appears.
+6. Game Over Condition: The game displays a game-over message when no more moves are available.
+7. Score Tracking: The score increases by the sum of all merged tiles during each move.
+8. Start/Restart Button: A button that starts the game or restarts it after the first move.
+9. Dynamic UI Updates: The game board updates in real-time based on the player’s actions.
+
+### Key Classes and Methods
+
+- Game Class: Located in src/modules/Game.class.js. Key methods include:
+
+- constructor(initialState): Initializes the game with an optional initial state.
+- getState(): Returns the current state of the game board.
+- getScore(): Returns the current score.
+- getStatus(): Returns the current status (e.g., ongoing, win, game over).
+- moveLeft(), moveRight(), moveUp(), moveDown(): Methods to move tiles in respective directions.
+- start(): Starts the game.
+- restart(): Restarts the game.
+
+### Usage Instructions
+
+- Setup: Ensure all project files are in the correct directories.
+- Start Game: Open index.html in a web browser to view the game interface.
+- Play Game: Use the arrow keys to move tiles and try to reach the 2048 tile.
+- Restart Game: Click the "Restart" button to reset the game at any time.
+
+#### Run Locally
+
+Clone the project
+
+```bash
+  git clone
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the development server with the following command
+
+```bash
+  npm start
+```
---
+++ src/modules/Game.class.js
@@ -0,0 +1,266 @@
+'use strict';
+
+/**
+ * This class represents the game.
+ * Now it has a basic structure, that is needed for testing.
+ * Feel free to add more props and methods if needed.
+ */
+class Game {
+  static gameStatuses = {
+    idle: 'idle',
+    playing: 'playing',
+    lose: 'lose',
+    win: 'win',
+  };
+
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.initialState = initialState;
+    this.state = this.initialState.map((row) => [...row]);
+    this.status = Game.gameStatuses.idle;
+    this.score = 0;
+  }
+
+  moveLeft() {
+    if (this.getStatus() !== Game.gameStatuses.playing) {
+      return;
+    }
+
+    const updatedState = this.state.map((row) => this.move(row));
+
+    if (JSON.stringify(this.getState()) !== JSON.stringify(updatedState)) {
+      this.updateGameState(updatedState);
+      this.addCells();
+    }
+  }
+
+  moveRight() {
+    if (this.getStatus() !== Game.gameStatuses.playing) {
+      return;
+    }
+
+    const reverseState = this.state.map((row) => [...row].reverse());
+
+    const updatedState = reverseState.map((row) => {
+      return this.move(row).reverse();
+    });
+
+    if (JSON.stringify(this.getState()) !== JSON.stringify(updatedState)) {
+      this.updateGameState(updatedState);
+      this.addCells();
+    }
+  }
+
+  moveUp() {
+    if (this.getStatus() !== Game.gameStatuses.playing) {
+      return;
+    }
+
+    const rotateState = this.rotateMatrixCounteClockwise(this.getState());
+
+    const updatedState = rotateState.map((row) => this.move(row));
+
+    const unRotateSteate = this.rotateMatrixClockwise(updatedState);
+
+    if (JSON.stringify(this.getState()) !== JSON.stringify(unRotateSteate)) {
+      this.updateGameState(unRotateSteate);
+      this.addCells();
+    }
+  }
+
+  moveDown() {
+    if (this.getStatus() !== Game.gameStatuses.playing) {
+      return;
+    }
+
+    const rotateState = this.rotateMatrixClockwise(this.getState());
+
+    const updatedState = rotateState.map((row) => this.move(row));
+
+    const unRotateSteate = this.rotateMatrixCounteClockwise(updatedState);
+
+    if (JSON.stringify(this.getState()) !== JSON.stringify(unRotateSteate)) {
+      this.updateGameState(unRotateSteate);
+      this.addCells();
+    }
+  }
+
+  move(row) {
+    const newRow = [];
+    let i = 0;
+
+    while (i < row.length) {
+      const current = row[i];
+
+      if (current) {
+        let pushed = false;
+
+        for (let j = i + 1; j < row.length; j++) {
+          const next = row[j];
+
+          if (next === current) {
+            newRow.push(current * 2);
+            this.updateGameScore(current * 2);
+            pushed = true;
+            i = j + 1;
+            break;
+          } else if (next) {
+            newRow.push(current);
+            pushed = true;
+            i = j;
+            break;
+          }
+        }
+
+        if (!pushed) {
+          newRow.push(current);
+          i++;
+        }
+      } else {
+        i++;
+      }
+    }
+
+    while (newRow.length < row.length) {
+      newRow.push(0);
+    }
+
+    return newRow;
+  }
+
+  rotateMatrixClockwise(matrix) {
+    const n = matrix.length;
+    const rotatedMatrix = [];
+
+    for (let i = 0; i < n; i++) {
+      rotatedMatrix.push([]);
+
+      for (let j = 0; j < n; j++) {
+        rotatedMatrix[i].unshift(matrix[j][i]);
+      }
+    }
+
+    return rotatedMatrix;
+  }
+
+  rotateMatrixCounteClockwise(matrix) {
+    const n = matrix.length;
+    const rotatedMatrix = [];
+
+    for (let i = 0; i < n; i++) {
+      rotatedMatrix.unshift([]);
+
+      for (let j = 0; j < n; j++) {
+        rotatedMatrix[0].push(matrix[j][i]);
+      }
+    }
+
+    return rotatedMatrix;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  start() {
+    this.status = Game.gameStatuses.playing;
+    this.addCells(2);
+  }
+
+  restart() {
+    this.status = Game.gameStatuses.idle;
+    this.resetState();
+  }
+
+  getEmptyCells() {
+    return this.state
+      .flatMap((row, rowIndex) => {
+        return row.map((cell, colIndex) => {
+          return cell === 0 ? [rowIndex, colIndex] : null;
+        });
+      })
+      .filter((cell) => cell !== null);
+  }
+
+  createNewTile() {
+    const emptyCells = this.getEmptyCells();
+
+    if (!emptyCells.length) {
+      return;
+    }
+
+    const [row, col] =
+      emptyCells[Math.trunc(Math.random() * emptyCells.length)];
+
+    this.state[row][col] = Math.random() < 0.9 ? 2 : 4;
+  }
+
+  addCells(count = 1) {
+    for (let i = 0; i < count; i++) {
+      this.createNewTile();
+    }
+
+    const state = this.getState();
+
+    if (this.isVictory(state)) {
+      this.status = Game.gameStatuses.win;
+    } else if (this.isGameOver(state)) {
+      this.status = Game.gameStatuses.lose;
+    }
+  }
+
+  resetState() {
+    this.state = this.initialState.map((row) => [...row]);
+    this.score = 0;
+  }
+
+  isGameOver(currentState) {
+    if (this.status !== Game.gameStatuses.playing) {
+      return false;
+    }
+
+    for (let row = 0; row < currentState.length; row++) {
+      for (let col = 0; col < currentState[row].length; col++) {
+        if (
+          currentState[row][col] === 0 ||
+          (col < currentState[row].length - 1 &&
+            currentState[row][col] === currentState[row][col + 1]) ||
+          (row < currentState.length - 1 &&
+            currentState[row][col] === currentState[row + 1][col])
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  updateGameState(state) {
+    this.state = state;
+  }
+
+  updateGameScore(scoreToAdd) {
+    this.score += scoreToAdd;
+  }
+
+  isVictory(state) {
+    return state.flat().some((tile) => tile === 2048);
+  }
+}
+
+module.exports = Game;
